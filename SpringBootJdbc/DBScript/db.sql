bccd0a46ed4a8ddbb3b86ef3c296a80b4f5efbe8
Tushar <gupta.tus@husky.neu.edu>
2018-11-17 00:32:35 -0500

    test merge

--- SpringBootJdbc/DBScript/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `database`.`vehicle` (
-  `vin` VARCHAR(45) NOT NULL,
-  `make` VARCHAR(45) NOT NULL,
-  `model` VARCHAR(45) NOT NULL,
-  `year` INT NOT NULL,
-  `redlineRpm` INT NOT NULL,
-  `maxFuelVolume` INT NOT NULL,
-  `lastServiceDate` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`vin`));
